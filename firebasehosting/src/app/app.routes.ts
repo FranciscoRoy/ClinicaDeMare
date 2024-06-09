e47978d01405040f4ca4402ed2e9d53094fb6577
Roy <franciscoroy.dm@gmail.com>
2024-06-09 20:06:53 -0300

    creoqueestoelimina

--- firebasehosting/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { InicioComponent } from './inicio/inicio.component';
-import { ExtraComponent } from './extra/extra.component';
-
-export const routes: Routes = [];
-
